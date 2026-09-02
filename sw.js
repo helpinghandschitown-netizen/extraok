6dcac66ba3067701898feb3ee836a3884dbbef43
helpinghandschitown-netizen <241166122+helpinghandschitown-netizen@users.noreply.github.com>
2026-09-02 12:56:21 -0500

    Refresh offline cache for setup offer

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='extraok-v1.1';
+const CACHE='extraok-v1.2';
 const ASSETS=['./','./index.html','./styles.css','./src/app.mjs','./src/core.mjs','./src/crypto.mjs','./src/store.mjs','./src/license.mjs','./manifest.webmanifest','./assets/icon.svg','./privacy.html','./terms.html','./change-order-approval-checklist.html'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
